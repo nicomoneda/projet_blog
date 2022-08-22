80318d1576b94e606e651c0e09425c109ec49c01
A Sathiesh <satwebfr@gmail.com>
2022-08-22 15:19:11 +0200

    feat: Home page avec Title Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { HomeComponent } from './layout/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogsComponent
+    BlogsComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
